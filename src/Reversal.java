fb479326be0997b16e4a70f1e09d931e8cde3d26
Marc C <marc.charette.11@cnu.edu>
2015-10-20 08:40:24 -0400

    Whoops recreated project twice, still getting used to addon to IntelliJ. This is first attempt.

---
+++ src/Reversal.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Reversal {
+    public static void reverseFile(File input, File output) throws FileNotFoundException{
+        String line;
+
+        ArrayList<String> outputList = new ArrayList<String>();
+
+        try {
+
+            FileReader fileReader =
+                    new FileReader(input);
+
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                outputList.add(line);
+            }
+
+            bufferedReader.close();
+
+            FileWriter fileWriter =
+                    new FileWriter(output);
+            BufferedWriter bufferedWriter =
+                    new BufferedWriter(fileWriter);
+            for (int i = outputList.size() - 1; i >= 0; i--) {
+                bufferedWriter.write(new StringBuilder(outputList.get(i)).reverse().toString());
+                System.out.println();
+            }
+
+            bufferedWriter.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            input + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + input + "'");
+        }
+    }
+
+    public static void main(String[] args){
+        File input = new File("C:\\Users\\mb_ch\\Documents\\Test.txt");
+        File output = new File("C:\\Users\\mb_ch\\Documents\\Output.txt");
+        try {
+            reverseFile(input, output);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+    }
+}
