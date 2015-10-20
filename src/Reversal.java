56756266e204fc0f9f2b2dc6779fff3cb9d5e988
Marc C <marc.charette.11@cnu.edu>
2015-10-20 17:22:56 -0400

    Added a scanner to read individual parts from each line the bufferedReader gets

--- src/Reversal.java
+++ src/Reversal.java
@@ -1,33 +1,42 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Reversal {
     public static void reverseFile(File input, File output) throws FileNotFoundException{
         String line;
-
-        ArrayList<String> outputList = new ArrayList<String>();
+        ArrayList<String> lineList = new ArrayList<>();
+        ArrayList<String> outputList = new ArrayList<>();
 
         try {
-
             FileReader fileReader =
                     new FileReader(input);
+
+            FileWriter fileWriter =
+                    new FileWriter(output);
 
             BufferedReader bufferedReader =
                     new BufferedReader(fileReader);
 
+            BufferedWriter bufferedWriter =
+                    new BufferedWriter(fileWriter);
+
             while ((line = bufferedReader.readLine()) != null) {
-                outputList.add(line);
+                Scanner in = new Scanner(line);
+                while(in.hasNext()){
+                    outputList.add(in.next() + " ");
+                }
+                outputList.add(System.getProperty("line.separator"));
             }
+
 
             bufferedReader.close();
 
-            FileWriter fileWriter =
-                    new FileWriter(output);
-            BufferedWriter bufferedWriter =
-                    new BufferedWriter(fileWriter);
+
+
+
             for (int i = outputList.size() - 1; i >= 0; i--) {
                 bufferedWriter.write(new StringBuilder(outputList.get(i)).toString());
-                bufferedWriter.append(System.getProperty("line.separator"));
             }
 
             bufferedWriter.close();
