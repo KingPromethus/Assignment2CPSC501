522fec8db2c097a2bb041234f9810debeef35cd4
Marc C <marc.charette.11@cnu.edu>
2015-10-20 18:14:57 -0400

    Fixed the extra whitespace problems in the output and created test method

--- src/Reversal.java
+++ src/Reversal.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Reversal {
     public static void reverseFile(File input, File output) throws FileNotFoundException{
         String line;
-        ArrayList<String> lineList = new ArrayList<>();
         ArrayList<String> outputList = new ArrayList<>();
 
         try {
@@ -23,20 +22,20 @@ public class Reversal {
 
             while ((line = bufferedReader.readLine()) != null) {
                 Scanner in = new Scanner(line);
+                outputList.add(in.next());
                 while(in.hasNext()){
                     outputList.add(in.next() + " ");
                 }
                 outputList.add(System.getProperty("line.separator"));
             }
-
-
+            outputList.remove(outputList.size()-1);
             bufferedReader.close();
 
 
 
 
             for (int i = outputList.size() - 1; i >= 0; i--) {
-                bufferedWriter.write(new StringBuilder(outputList.get(i)).toString());
+                bufferedWriter.write(outputList.get(i));
             }
 
             bufferedWriter.close();
